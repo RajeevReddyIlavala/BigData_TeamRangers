9ba37f612f4a8d0069af9561cc66c6ec56c24c3e
smit sheth <smitsheth10@yahoo.com>
2019-11-09 23:49:22 -0500

    Add implementation of finding top 5 frequent elements and distinct values in column. Also implementation for storing output in json file

--- task1.py
+++ task1.py
@@ -4,6 +4,7 @@ from pyspark.sql import SparkSession
 from csv import reader
 import sys
 from pyspark.sql import functions as F
+import json 
 
 spark = SparkSession \
      .builder \
@@ -13,20 +14,40 @@ df = spark.read.csv(sys.argv[1], sep = '\t',header = 'true')
 
 table1 = df.replace("No Data",'null').replace('n/a','null').replace('NA','null').replace('-','null').replace('None','null')
 table1.createOrReplaceTempView("table1")
-nonemptycells = 0
-emptycells = 0
-for c in df.columns:
-	print(c)	
-	result = spark.sql("SELECT count(`"+c+"`) as cellcount from table1 where `"+c+ "`<> 'null' and `"+c+ "` is not null ")
+
+
+result_json = {
+    "dataset_name": sys.argv[1].split('/')[-1]
+    }
+result_json["columns"]=list()
+
+for column in df.columns:	
+	nonemptycells = 0
+	emptycells = 0
+	result = spark.sql("SELECT COUNT(`"+column+"`) as cellcount from table1 WHERE `"+column+ "`<> 'null' and `"+column+ "` is not null ")
 	nonempty = result.collect()	
-	nonemptycells += nonempty[0].cellcount
-	result = spark.sql("SELECT count(`"+c+"`) as cellcount from table1 where `"+c+ "`= 'null' or `"+c+ "` is  null ")
+	nonemptycells = nonempty[0].cellcount
+	result = spark.sql("SELECT COUNT(`"+column+"`) as cellcount from table1 WHERE `"+column+ "`= 'null' or `"+column+ "` is  null ")
 	empty = result.collect()
-	emptycells += empty[0].cellcount
-	
+	emptycells = empty[0].cellcount
+	result = spark.sql("SELECT COUNT(DISTINCT `"+column+"`) as distinct_count from table1")
+	distinct_values=result.collect()[0].distinct_count
+	result = spark.sql("SELECT `"+column+"` as attr, COUNT(`"+column+"`) as frequency from table1 GROUP BY `"+column+"` ORDER BY frequency DESC LIMIT 5")
+	top_frequent_elements = [row.attr for row in result.collect()]
+
+	column_json={
+		"column_name": column,
+		"number_non_empty_cells":nonemptycells,
+		"number__empty_cells":emptycells,
+		"number_distinct_values":distinct_values,
+		"frequent_values":top_frequent_elements
+	}
+	result_json["columns"].append(column_json)
+
+filename=sys.argv[1].split('/')[-1]+ ".json"
+
+with open(filename, 'w') as f:
+    json.dump(result_json, f)
 
 
-print(nonemptycells)
-print(emptycells)
-#result = spark.sql("SELECT * from table1").show()
 spark.stop()
