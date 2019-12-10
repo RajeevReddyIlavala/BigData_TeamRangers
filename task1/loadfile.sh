a18ed0459bfcefff9c3a1a943e993e8a200e58e5
Akshat Khare <akshatkhare@10-18-171-247.dynapool.wireless.nyu.edu>
2019-12-10 18:23:35 -0500

    final push

--- task1/loadfile.sh
+++ task1/loadfile.sh
@@ -7,6 +7,6 @@ cat datasets.tsv | while read line ; do
 read -a name <<< "$line"
 filename="${name}.tsv.gz"
 echo $name
-spark-submit --conf spark.pyspark.python=$PYSPARK_PYTHON task1.py /user/hm74/NYCOpenData/$filename
+spark-submit --conf spark.pyspark.python=$PYSPARK_PYTHON nullValues.py /user/hm74/NYCOpenData/$filename
 done 
 
